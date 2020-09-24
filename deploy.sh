7b02066d9b8b25daa516c7efc5df0495e8ebb70a
Cody Swendrowski <cody@ironmoosedevelopment.com>
2020-09-24 10:39:41 -0500

    Remove submodule

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,14 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+#git init
+#git add -A
+#git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:League-of-Foundry-Developers/FoundryGet-PackageBrowser.git master:gh-pages
+#git push -f git@github.com:League-of-Foundry-Developers/FoundryGet-PackageBrowser.git master:gh-pages
 
 cd -
