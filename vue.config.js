88d42f9ce1283de26bbd04e1f4fdd81e749633b4
ardittristan <ardittristan@gmail.com>
2020-10-29 00:12:55 +0100

    decrease vuetify size

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,11 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   publicPath: process.env.NODE_ENV === 'production'
     ? '/FoundryGet-PackageBrowser/'
-    : '/'
+    : '/',
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === "production") {config.module.rule('vue')
+        .use('vuetify-loader')
+          .loader('vuetify-loader')
+    }
+  }
 };
