be19401f320e61b7e6304e6eae9967cbf87c64f9
ardittristan <ardittristan@gmail.com>
2020-11-03 20:57:16 +0100

    actual update, oops

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,20 +5,21 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
 	theme: {
-		dark: true,
-		options: {
-			customProperties: true,
-		},
-		themes: {
-			dark: {
-				primary: '#555555',
-				secondary: '#424242',
-				accent: '#666666',
-				error: '#FF5252',
-				info: '#666666',
-				success: '#4CAF50',
-				warning: '#FFC107',
-			}
-		}
+		disable: true
+		// dark: true,
+		// options: {
+		// 	customProperties: true,
+		// },
+		// themes: {
+		// 	dark: {
+		// 		primary: '#555555',
+		// 		secondary: '#424242',
+		// 		accent: '#666666',
+		// 		error: '#FF5252',
+		// 		info: '#666666',
+		// 		success: '#4CAF50',
+		// 		warning: '#FFC107',
+		// 	}
+		// }
 	}
 });
